a4fc296aabca43bae99c258d50e54d71c63176d1
Ziad El-Rady <92187671+ZiadEl-Rady@users.noreply.github.com>
2026-01-17 02:23:24 -0500

    player no longer moves when there's no input thx to Jane

--- Roomba/Assets/Scripts/PlayerController.cs
+++ Roomba/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,8 @@
 using MoreMountains.Feedbacks;
+using NUnit.Framework.Internal.Commands;
+using System.Collections;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,23 +16,27 @@ public class PlayerController : MonoBehaviour
     Rigidbody _rb;
 
     bool _canMove = true;
+    Coroutine _canMoveCoroutine;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _canMove = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (_canMove == false)
+        {
+            return;
+        }
         //TODO: if selected connect the input otherwise disable it
         float signedAngle = Vector2.SignedAngle(_movementVector, Vector2.up) + _northYRotation; //offset this by the rotation
-        //float currentYRot = transform.rotation.eulerAngles.y - 180 + _northYRotation;
         
         Quaternion desiredQuat = Quaternion.Euler(0, signedAngle, 0);
-        //Debug.Log($"Y: {transform.rotation.y}. desiredY: {desiredQuat.y}");
         if (transform.rotation.y > desiredQuat.y + 0.02 || transform.rotation.y < desiredQuat.y - 0.02) //if not in the tight range of target angle, keep rotating
         {
             var step = _rotateSpeed * Time.deltaTime;
@@ -39,14 +46,21 @@ public class PlayerController : MonoBehaviour
             _rb.AddForce(transform.forward * _movementSpeed);
         }
     }
-    void OnMove(InputValue inputValue)
+    public void OnMove(InputAction.CallbackContext ctx)
     {
         //TO DO: check gameManager's selectionmanager to get selected status
 
-        _movementVector = inputValue.Get<Vector2>();
-        
-        
+        _canMove = true;
+        if (ctx.canceled)
+        {
+            _canMove = false;
+        }
+        Debug.Log($"movement vector: {_movementVector}");
+
+
+        _movementVector = ctx.ReadValue<Vector2>();        
     }
+
     private void OnEnable()
     {
         EnableInput();
