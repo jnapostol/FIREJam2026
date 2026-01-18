714e24f9e09a3cf17d73391b117d186618ad2898
Ziad El-Rady <92187671+ZiadEl-Rady@users.noreply.github.com>
2026-01-18 05:12:24 -0500

    gave player less friction

--- Roomba/Assets/Scripts/TableLeg.cs
+++ Roomba/Assets/Scripts/TableLeg.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 public class TableLeg : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
+    bool _hasPlayed;
+    public GameObject Sword;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Sword")
+        if (collision.gameObject.name.Contains("Sword") && _hasPlayed == false)
         {
             _anim.Play("Fall");
-            InventoryManager.Instance.RemoveCurrentAttachment();
+            //InventoryManager.Instance.RemoveCurrentAttachment();
+            if (Sword != null)
+            {
+                Sword.SetActive(false);
+            }
+            
+            _hasPlayed = true;
         }
     }
 }
